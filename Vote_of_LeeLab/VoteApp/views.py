f0334019f2bff9892867f8fad432fc4b08abfa98
Gusam Park <tnara9573@gmail.com>
2018-07-14 00:09:59 +0900

    update
    1. 템플릿 변경
    2. 400, 403, 404, 500 에러처리
    3. 디버깅 모드 해제

--- Vote_of_LeeLab/VoteApp/views.py
+++ Vote_of_LeeLab/VoteApp/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-import datetime, time
+from django.shortcuts import render, render_to_response, redirect
+from django.template import RequestContext
+import datetime, time, locale
 
 # Create your views here.
 
 # CountDown
+locale.setlocale(locale.LC_CTYPE, 'korean')
 COUNTDOWN_TARGET_DATE = datetime.datetime(2018, 8, 24, 00, 00, 00, 000000)
+DURIONG_DATE = datetime.timedelta(days=6, seconds=86399)
 
 def index(request):
     if COUNTDOWN_TARGET_DATE > datetime.datetime.now():
@@ -14,15 +17,48 @@ def index(request):
             request,
             "VoteApp/index.html",
            {
+               'active_home' : "active",
                'title' : "투표까지 남은시간",
+               'url' : "http://210.119.85.76/",
+               'description' : "LEE LAB 투표 예정일",
+               'sitename' : "Lee Lab Server",
+               'locale' : "ko_KR",
+               'type' : "article",
+               'date_of_begin_time' : "투표 예정일 : " + COUNTDOWN_TARGET_DATE.strftime('%Y년 %m월 %d일 - %H시 %M분 %S초'),
                'server_time' : send_data
            }
+        )
+    elif (COUNTDOWN_TARGET_DATE + DURIONG_DATE) > datetime.datetime.now():
+        td = (COUNTDOWN_TARGET_DATE + DURIONG_DATE) - datetime.datetime.now()
+        send_data = (td.days * 86400) + td.seconds
+        return render(
+            request,
+            "VoteApp/index.html",
+           {
+               'active_home' : "active",
+               'title' : "투표 종료까지 남은시간!",
+               'url' : "http://210.119.85.76/",
+               'description' : "LEE LAB 투표 진행중!",
+               'sitename' : "Lee Lab Server",
+               'locale' : "ko_KR",
+               'type' : "article",
+               'date_of_begin_time' : "투표 종료일 : " + (COUNTDOWN_TARGET_DATE + DURIONG_DATE).strftime('%Y년 %m월 %d일 - %H시 %M분 %S초'),
+                'server_time': send_data
+            }
         )
     else:
         return render(
             request,
             "VoteApp/index.html",
            {
+               'active_home' : "active",
+               'title' : "투표 종료",
+               'url' : "http://210.119.85.76/",
+               'description' : "LEE LAB 투표 결과",
+               'sitename' : "Lee Lab Server",
+               'locale' : "ko_KR",
+               'type' : "article",
+               'date_of_begin_time' : "투표 종료일 : " + (COUNTDOWN_TARGET_DATE + DURIONG_DATE).strftime('%Y년 %m월 %d일 - %H시 %M분 %S초'),
                 'server_time': 0
             }
         )
@@ -33,7 +69,24 @@ def about(request):
         request,
         "VoteApp/about.html",
         {
+            'active_about' : "active",
             'title' : "도움말",
             'content' : "테스트 페이지"
         }
     )
+
+
+def handler400(request, exception, template_name='VoteApp/400.html'):
+    return render(request, template_name, status=400)
+
+
+def handler403(request, exception, template_name='VoteApp/403.html'):
+    return render(request, template_name, status=403)
+
+
+def handler404(request, exception, template_name='VoteApp/404.html'):
+    return render(request, template_name, status=404)
+
+
+def handler500(request, exception, template_name='VoteApp/500.html'):
+    return render(request, template_name, status=500)
--- Vote_of_LeeLab/Vote_of_LeeLab/urls.py
+++ Vote_of_LeeLab/Vote_of_LeeLab/urls.py
@@ -3,7 +3,8 @@ Definition of urls for Vote_of_LeeLab.
 """
 
 from datetime import datetime
-from django.conf.urls import url
+from django.conf.urls import url, handler400, handler403, handler404, handler500
+from django.contrib.staticfiles.views import serve
 import django.contrib.auth.views
 
 import app.forms
@@ -14,6 +15,11 @@ import VoteApp.views
 # from django.conf.urls import include
 # from django.contrib import admin
 # admin.autodiscover()
+
+handler400 = 'VoteApp.views.handler400'
+handler403 = 'VoteApp.views.handler403'
+handler404 = 'VoteApp.views.handler404'
+handler500 = 'VoteApp.views.handler500'
 
 urlpatterns = [
     # Examples:
@@ -40,6 +46,9 @@ urlpatterns = [
             'next_page': '/',
         },
         name='logout'),
+
+    # if DEBUG set False, it needs.
+    url(r'^static/(?P<path>.*)$', serve, kwargs={'insecure': True}),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
