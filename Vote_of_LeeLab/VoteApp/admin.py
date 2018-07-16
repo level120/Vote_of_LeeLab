065074ffc85a9ca07fe6987bd610873e7c302f9b
Gusam Park <tnara9573@gmail.com>
2018-07-17 03:04:09 +0900

    register on admin
    admin에 모델 등록

--- Vote_of_LeeLab/VoteApp/admin.py
+++ Vote_of_LeeLab/VoteApp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from VoteApp.models import Like
+from VoteApp.models import Like, DisLike
 
 # Register your models here.
 admin.site.register(Like)
+admin.site.register(DisLike)
 #@admin.register(Like)
 #class PostAdmin(admin.ModelAdmin):
 #    list_display = ['name_id', 'like', 'dislike']
