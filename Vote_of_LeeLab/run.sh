6e0524eb60896f5a479fd4d0b9cc300bff28685e
Gusam Park <tnara9573@gmail.com>
2018-07-17 19:17:02 +0900

    reset 스크립트
    db 충돌문제로 인한 reset 스크립트 추가

--- Vote_of_LeeLab/run.sh
+++ Vote_of_LeeLab/run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pip3 install -r /var/www/requirements.txt
-#rm /var/www/db.sqlite3
-#find . -path "/var/www/*/migrations/*.py" -not -name "__init__.py" -delete
-#find . -path "/var/www/*/migrations/*.pyc"  -delete
+rm /var/www/db.sqlite3
+find /var/www/ -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find /var/www/ -path "*/migrations/*.pyc" -delete
 python3 /var/www/manage.py makemigrations
 python3 /var/www/manage.py migrate
 service apache2 restart
