3867cb9f26b4f69b36358a6a9a93fd96501ec63b
Gusam Park <tnara9573@gmail.com>
2018-07-17 19:40:39 +0900

    change path

--- Vote_of_LeeLab/run.sh
+++ Vote_of_LeeLab/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-rm /var/www/db.sqlite3
-find /var/www/ -path "*/migrations/*.py" -not -name "__init__.py" -delete
-find /var/www/ -path "*/migrations/*.pyc" -delete
+rm ~/django/Vote_of_LeeLab/db.sqlite3
+find ~/django/Vote_of_LeeLab/ -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find ~/django/Vote_of_LeeLab/ -path "*/migrations/*.pyc" -delete
 
 python3 ~/django/Vote_of_LeeLab/manage.py makemigrations
 python3 ~/django/Vote_of_LeeLab/manage.py migrate
